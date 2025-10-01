3705e47ad687ba349c83699b1fa50f5611fea7cb
105624033 <behzad.b.i.g@gmail.com>
2025-10-01 13:24:57 +0330

    models commit

---
+++ ElderlyHealthMonitor.ML/Models/FallData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML.Data;
+
+namespace ElderlyHealthMonitor.ML.Models
+{
+    public class FallData
+    {
+        [LoadColumn(0)]
+        public bool PreviousFall { get; set; }
+
+        [LoadColumn(1)]
+        public bool FOG { get; set; }
+
+        [LoadColumn(2)]
+        public bool SlowGait { get; set; }
+
+        [LoadColumn(3)]
+        [ColumnName("Label")]
+        public bool IsFall { get; set; }
+    }
+}
