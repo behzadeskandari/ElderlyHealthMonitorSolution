66f2a544c5722de35f7fef56273953d78c5cbab3
105624033 <behzad.b.i.g@gmail.com>
2025-10-06 13:36:33 +0330

    Sensor Data

--- ElderlyHealthMonitor.Edge/Interfaces/IDeviceAdapter.cs
+++ ElderlyHealthMonitor.Edge/Interfaces/IDeviceAdapter.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ElderlyHealthMonitor.Edge.Models;
 
 namespace ElderlyHealthMonitor.Edge.Interfaces
 {
-    internal interface IDeviceAdapter
+    public interface IDeviceAdapter : IDisposable
     {
+        /// <summary>
+        /// شروع اتصال و خواندن داده‌ها از دستگاه
+        /// </summary>
+        Task StartAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// رویدادی که وقتی داده جدید آمد فراخوانی می‌شود
+        /// </summary>
+        event EventHandler<SensorData> OnSensorData;
+
+        /// <summary>
+        /// آیا adapter به دستگاه وصل است
+        /// </summary>
+        bool IsConnected { get; }
     }
 }
